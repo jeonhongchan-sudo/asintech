eb342c1610f3e5be2e749ab0881888ffcfe332d8
jeonhongchan-sudo <jeonhongchan-sudo@users.noreply.github.com>
2026-02-15 12:42:35 +0900

    Update Action files (convert_r2, yml, requirements)

--- convert_r2.py
+++ convert_r2.py
@@ -533,7 +533,7 @@ def convert_to_pmtiles():
         "--force",
         "--no-line-simplification",
         "--no-tiny-polygon-reduction",
-        "-r1.5"
+        "-r1.3"
     ]
     
     has_input = False
